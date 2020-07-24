198c6bd4cee02ffeb23bcaeb07dadd18692883f0
daniel-kasabov <danielkasabov@abv.bg>
2020-07-24 11:36:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
-import pathlib
 
 
 # ---------------------------- Set up raw data ---------------------------
 # ========================================================================
 # 1. F O R  G D P   F O R E C A S T   G R A P H
-
+proxy = {
+    'http': 'http://172.27.0.20:8080',
+    'https': 'http://172.27.0.20:8080',
+}
 
     # historical data
-hist = pd.read_excel('https://github.com/daniel-kasabov/dash/blob/master/forecast.xlsx', index_col=None, header=0)
+hist = pd.read_excel('https://github.com/daniel-kasabov/dash/raw/master/history.xlsx', index_col=None, header=0)
 
     # forecast data
-fct = pd.read_excel('https://github.com/daniel-kasabov/dash/blob/master/forecast.xlsx', index_col=None, header=0)
+fct = pd.read_excel('https://github.com/daniel-kasabov/dash/raw/master/forecast.xlsx', index_col=None, header=0)
     # available models
 available_models = list(fct['Model'].unique())
     # dates with available forecast periods
@@ -34,7 +36,7 @@ colors = ['#d62728', '#ff7f0e', '#1f77b4'][::-1]
 #colors_ncst = ['#37AB65', '#3DF735', '#AD6D70', '#EC2504', '#8C0B90', '#C0E4FF', '#27B502', '#7C60A8', '#CF95D7', '#145JKH']          
 colors_ncst = ['red', 'turquoise', 'gold', 'lime', 'deeppink','mediumblue', 'blanchedalmond', 'lightslategray', 'darkseagreen', 'olive', 'purple']          
 
-ncst = pd.read_excel('https://github.com/daniel-kasabov/dash/blob/master/nowcast.xlsx', index_col=0, header=0)
+ncst = pd.read_excel('https://github.com/daniel-kasabov/dash/raw/master/nowcast.xlsx', index_col=0, header=0)
 ncst_period = ncst.index
 
 categ = ncst.columns.to_list()
